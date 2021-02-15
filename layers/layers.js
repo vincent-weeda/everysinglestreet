c6b78eb46cc422a92da962d323468b4276d03133
vincent-weeda <vincentweeda@gmail.ocm>
2021-02-15 10:08:26 +0200

    oulunkylä done

--- layers/layers.js
+++ layers/layers.js
@@ -1,5 +1,5 @@
 ol.proj.proj4.register(proj4);
-ol.proj.get("EPSG:3067").setExtent([379026.013812, 6668712.100452, 404147.984483, 6685843.286754]);
+ol.proj.get("EPSG:3067").setExtent([378500.996870, 6668888.243691, 405410.084116, 6687238.101385]);
 var wms_layers = [];
 
 
@@ -88,19 +88,19 @@ var lyr_photos_5 = new ol.layer.Vector({
 lyr_basemap_0.setVisible(true);lyr_routes_1.setVisible(true);lyr_missedstreets_2.setVisible(true);lyr_helsinkiborder_3.setVisible(true);lyr_gps_tracks_4.setVisible(true);lyr_photos_5.setVisible(true);
 var layersList = [lyr_basemap_0,lyr_routes_1,lyr_missedstreets_2,lyr_helsinkiborder_3,lyr_gps_tracks_4,lyr_photos_5];
 lyr_routes_1.set('fieldAliases', {'fid': 'fid', 'Area': 'Area', 'Status': 'Status', 'Distance': 'Distance', });
-lyr_missedstreets_2.set('fieldAliases', {'Name': 'Name', 'description': 'description', 'timestamp': 'timestamp', 'begin': 'begin', 'end': 'end', 'altitudeMode': 'altitudeMode', 'tessellate': 'tessellate', 'extrude': 'extrude', 'visibility': 'visibility', 'drawOrder': 'drawOrder', 'icon': 'icon', });
+lyr_missedstreets_2.set('fieldAliases', {'fid': 'fid', });
 lyr_helsinkiborder_3.set('fieldAliases', {'fid': 'fid', });
-lyr_gps_tracks_4.set('fieldAliases', {'fid': 'fid', 'Area': 'Area', 'Distance': 'Distance', 'Date': 'Date', 'Time': 'Time', 'Pace': 'Pace', 'Elevation_number': 'Elevation_number', 'Elevation': 'Elevation', 'Link': 'Link', 'layer': 'layer', 'path': 'path', });
+lyr_gps_tracks_4.set('fieldAliases', {'fid': 'fid', 'Area': 'Area', 'Distance': 'Distance', 'Date': 'Date', 'Time': 'Time', 'Pace': 'Pace', 'Elevation_number': 'Elevation_number', 'Elevation': 'Elevation', 'Link': 'Link', });
 lyr_photos_5.set('fieldAliases', {'fid': 'fid', 'altitude': 'altitude', 'direction': 'direction', 'longitude': 'longitude', 'latitude': 'latitude', 'timestamp': 'timestamp', 'Date': 'Date', 'photo_html': 'photo_html', });
 lyr_routes_1.set('fieldImages', {'fid': 'Hidden', 'Area': 'TextEdit', 'Status': 'TextEdit', 'Distance': 'TextEdit', });
-lyr_missedstreets_2.set('fieldImages', {'Name': 'Hidden', 'description': 'Hidden', 'timestamp': 'Hidden', 'begin': 'Hidden', 'end': 'Hidden', 'altitudeMode': 'Hidden', 'tessellate': 'Hidden', 'extrude': 'Hidden', 'visibility': 'Hidden', 'drawOrder': 'Hidden', 'icon': 'Hidden', });
+lyr_missedstreets_2.set('fieldImages', {'fid': '', });
 lyr_helsinkiborder_3.set('fieldImages', {'fid': 'Hidden', });
-lyr_gps_tracks_4.set('fieldImages', {'fid': 'Hidden', 'Area': 'TextEdit', 'Distance': 'TextEdit', 'Date': 'DateTime', 'Time': 'TextEdit', 'Pace': 'TextEdit', 'Elevation_number': 'Hidden', 'Elevation': 'TextEdit', 'Link': 'TextEdit', 'layer': 'Hidden', 'path': 'Hidden', });
+lyr_gps_tracks_4.set('fieldImages', {'fid': 'Hidden', 'Area': 'TextEdit', 'Distance': 'TextEdit', 'Date': 'DateTime', 'Time': 'TextEdit', 'Pace': 'TextEdit', 'Elevation_number': 'Range', 'Elevation': 'TextEdit', 'Link': 'TextEdit', });
 lyr_photos_5.set('fieldImages', {'fid': 'Hidden', 'altitude': 'Hidden', 'direction': 'Hidden', 'longitude': 'Hidden', 'latitude': 'Hidden', 'timestamp': 'Hidden', 'Date': 'DateTime', 'photo_html': 'TextEdit', });
 lyr_routes_1.set('fieldLabels', {'Area': 'inline label', 'Status': 'inline label', 'Distance': 'inline label', });
-lyr_missedstreets_2.set('fieldLabels', {});
+lyr_missedstreets_2.set('fieldLabels', {'fid': 'no label', });
 lyr_helsinkiborder_3.set('fieldLabels', {});
-lyr_gps_tracks_4.set('fieldLabels', {'Area': 'inline label', 'Distance': 'inline label', 'Date': 'inline label', 'Time': 'inline label', 'Pace': 'inline label', 'Elevation': 'inline label', 'Link': 'inline label', });
+lyr_gps_tracks_4.set('fieldLabels', {'Area': 'no label', 'Distance': 'no label', 'Date': 'no label', 'Time': 'no label', 'Pace': 'no label', 'Elevation_number': 'no label', 'Elevation': 'no label', 'Link': 'no label', });
 lyr_photos_5.set('fieldLabels', {'Date': 'no label', 'photo_html': 'no label', });
 lyr_photos_5.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
