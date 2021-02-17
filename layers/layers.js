c252304a200f29bbe493772df1218212c14aebb2
vincent-weeda <vincentweeda@gmail.ocm>
2021-02-17 07:25:24 +0200

    sepänmäki route

--- layers/layers.js
+++ layers/layers.js
@@ -1,5 +1,5 @@
 ol.proj.proj4.register(proj4);
-ol.proj.get("EPSG:3067").setExtent([376557.647302, 6667082.314837, 405948.445943, 6687124.502357]);
+ol.proj.get("EPSG:3067").setExtent([376557.647302, 6662456.349406, 405948.445943, 6691750.467788]);
 var wms_layers = [];
 
 
