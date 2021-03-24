a1027dc59a1d5003df5b52df8a9ba4c0d393c641
vincent-weeda <vincentweeda@gmail.ocm>
2021-03-24 22:09:30 +0200

    herttoniemenranta done

--- styles/helsinkiborder_3_style.js
+++ styles/helsinkiborder_3_style.js
@@ -1,7 +1,7 @@
 var size = 0;
 var placement = 'point';
 
-var style_helsinkiborder_2 = function(feature, resolution){
+var style_helsinkiborder_3 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
