99c2ea1068cff52c580d6864a4c8c0e2d63221bf
vincent-weeda <vincentweeda@gmail.ocm>
2021-04-05 12:08:39 +0300

    ruoholahti next

--- styles/gpstracks_2_style.js
+++ styles/gpstracks_2_style.js
@@ -1,7 +1,7 @@
 var size = 0;
 var placement = 'point';
 
-var style_newroutesmarchapril_2 = function(feature, resolution){
+var style_gpstracks_2 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
@@ -21,7 +21,7 @@ var style_newroutesmarchapril_2 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(159,159,159,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 1}),
+        stroke: new ol.style.Stroke({color: 'rgba(84,176,74,1.0)', lineDash: null, lineCap: 'square', lineJoin: 'bevel', width: 2}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
