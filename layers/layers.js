cb6513ee436ba3dd59474e038bc8c8db81b62e31
vincent-weeda <vincentweeda@gmail.ocm>
2021-04-13 21:57:35 +0300

    kala

--- layers/layers.js
+++ layers/layers.js
@@ -1,5 +1,5 @@
 ol.proj.proj4.register(proj4);
-ol.proj.get("EPSG:3067").setExtent([377712.018846, 6668108.576810, 404676.603311, 6687423.633767]);
+ol.proj.get("EPSG:3067").setExtent([378448.512980, 6667411.102292, 405856.018873, 6687043.429208]);
 var wms_layers = [];
 
 
